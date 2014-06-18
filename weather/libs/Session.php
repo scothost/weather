1ad556e5d610f4191103dc11297141bcbc4f405a
vigh iosif <feed@vighiosif.ro>
2014-06-18 10:40:56 +0200

    working part 3.

---
+++ weather/libs/Session.php
@@ -0,0 +1,28 @@
+<?php
+
+class Session
+{
+	public static function init()
+	{
+		@session_start();
+		//echo 'funky session start';
+	}
+	
+	public static function set($key, $value)
+	{
+		$_SESSION[$key] = $value;
+	}
+	
+	public static function get($key)
+	{
+		if (isset($_SESSION[$key]))
+		return $_SESSION[$key];
+	}
+	
+	public static function destroy()
+	{
+		//unset($_SESSION);
+		session_destroy();
+	}
+	
+}
