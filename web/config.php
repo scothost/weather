41a328f97c9e4fbcd14958d82e8b5d186e4d9e79
vigh iosif <vighiosif@gmail.com>
2014-07-28 22:51:28 +0200

    Fucking config.php file works nicely :)

--- web/config.php
+++ web/config.php
@@ -7,23 +7,31 @@ define('MY_HASH_GENERAL_KEY','qqOS@__^^G,RP-.s;kWE$j*C8<F%5O|S');
 //This is for database passwords only
 define('MY_HASH_PASSWORD_KEY','$d]T8Dxp<|kG0!l+cg3 ooxCXX:;0w60');
 
-//Database ONLINE
+
+if ($_SERVER['SERVER_NAME'] == 'localhost')
+{
+	//Database LOCALHOST
+	define('DB_TYPE', 'mysql');
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'weather_DB');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+
+	// Paths
+	//Allways provide a trailing slash </> after a path
+	define('URL', 'http://localhost/weather/web/');
+} else {
+
+	//Database ONLINE
 	define('DB_TYPE', 'mysql');
 	define('DB_HOST', 'localhost');
 	define('DB_NAME', 'wwwvighi_weather');
 	define('DB_USER', 'wwwvighi_dev');
 	define('DB_PASS', '{9oUz?Dbr~5&');
 
-//Database LOCALHOST
-	// define('DB_TYPE', 'mysql');
-	// define('DB_HOST', 'localhost');
-	// define('DB_NAME', 'weather_DB');
-	// define('DB_USER', 'root');
-	// define('DB_PASS', '');
-	
-// 	PATHS
+	// Paths
+	//Allways provide a trailing slash </> after a path
+	define('URL', 'http://weather.vighiosif.ro/');
+}
 
-//Allways provide a trailing slash </> after a path
-define('URL', 'http://weather.vighiosif.ro/');
-// define('URL', 'http://localhost/weather/web/');
 define('LIBS', 'libs/');
